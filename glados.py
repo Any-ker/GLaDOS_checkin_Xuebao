8ab8a13469d8e8e5fff461c95515078e9e7b48d2
Any-ker <128891172+Any-ker@users.noreply.github.com>
2024-07-12 22:09:56 +0800

    Update glados.py

--- glados.py
+++ glados.py
@@ -37,6 +37,6 @@ if __name__ == '__main__':
             print('cookie已失效')  # 日志输出
      #--------------------------------------------------------------------------------------------------------#   
     if sckey != "":
-         requests.get('http://www.pushplus.plus/send?token=' + sckey + '&title='+'晚安雪宝~' + mess + '&content=' + sendContent)
+         requests.get('http://www.pushplus.plus/send?token=' + sckey + '&title='+'晚安雪宝~ 晚安洁宝:)' + mess + '&content=' + sendContent)
 
 
